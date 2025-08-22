ca294d70f7fb50f25bd2e74fd988db85249ae485
Mohamed Abdelmoneim <men3m1232006@gmail.com>
2025-08-22 12:00:27 +0300

    feat: add searching and filtering feature

---
+++ search and filter.js
@@ -0,0 +1,11 @@
+// let asideBtn = document.querySelector(".my-side-btn");
+// let asideIcon = document.querySelector(".aside-icon-btn")
+
+// asideBtn.addEventListener("click", function () {
+
+//     if (asideIcon.classList.contains("fa-chevron-left")) {
+//         asideIcon.classList.remove("fa-chevron-left")
+//         asideIcon.classList.add("fa-chevron-right")
+//     }
+
+// })
